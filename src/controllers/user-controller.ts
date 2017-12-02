6cd892eccf69341e36c0776743cdd049b25ac809
Michael Rojas <michaelrojasp@gmail.com>
2017-12-01 23:38:02 -0600

    Query database to retrieve an user by username

--- src/controllers/user-controller.ts
+++ src/controllers/user-controller.ts
@@ -1,8 +1,30 @@
+import { DBConnector } from '../db/db-connector';
+import { User } from '../models/user';
+
+const dbConnector = new DBConnector();
+const pool = dbConnector.getPool();
 
 export class UserController {
+
   public async getUser(req, res) {
-    // TODO: Query Postgre SQL DB to get the user
-    const user = {};
+    const usernameParam = req.params.username;
+    const query = {
+      name: 'fetch-user-by-username',
+      text: 'SELECT * FROM public.user WHERE username = $1',
+      values: [usernameParam],
+    };
+
+    const results = await pool.query(query);
+    const {
+      username,
+      first_name,
+      last_name,
+      email_address,
+      age,
+      id,
+    } = results.rows[0];
+    const user = new User(username, first_name, last_name, email_address, age);
     res.json(user);
   }
+
 }
